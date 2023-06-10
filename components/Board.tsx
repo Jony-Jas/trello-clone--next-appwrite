1d1a1081d33044b847bdcdc13737948a99a99848
Jony-Jas <jonyjasjonyjas@gmail.com>
2023-06-10 13:43:53 +0530

    setting zustand

--- components/Board.tsx
+++ components/Board.tsx
@@ -1,15 +1,27 @@
 "use client";
 
-import React from "react";
+import React, { useEffect } from "react";
+import { useBoardStore } from "@/store/BoardStore";
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
 
 function Board() {
+  const [board, getBoard] = useBoardStore((state) => [
+    state.board,
+    state.getBoard,
+  ]);
+
+  useEffect(() => {
+    console.log("Use effect");
+    getBoard();
+  },[getBoard]);
+
   return (
-    <DragDropContext>
-      <Droppable droppableId="board" direction="horizontal" type="column">
-        {(provided) => <div>Hello</div>}
-      </Droppable>
-    </DragDropContext>
+    <h1>Board</h1>
+    // <DragDropContext>
+    //   <Droppable droppableId="board" direction="horizontal" type="column">
+    //     {(provided) => <div>Hello</div>}
+    //   </Droppable>
+    // </DragDropContext>
   );
 }
 
